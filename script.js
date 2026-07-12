fbc91e23cbb31452043681b60f4c60781d1afae5
nimishamakwana233-web <nimishamakwana233@gmail.com>
2026-07-12 19:00:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Fade animation when opening Page 2
 
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function (e) {
+
+    e.preventDefault();
+
+    document.body.style.opacity = "0";
+
+    document.body.style.transition = "0.8s";
+
+    setTimeout(() => {
+
+        window.location.href = "page2.html";
+
+    }, 800);
+
+});
